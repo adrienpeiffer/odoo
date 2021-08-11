bd83df4eb27002d31222b3fdd38ce8e21254f7a8
François Honoré <francois.honore@acsone.eu>
2021-08-11 15:14:01 +0200

    [FIX] Stock Account: do not create cost anglo saxon move lines if no sale purchase line

--- addons/purchase_stock/models/account_invoice.py
+++ addons/purchase_stock/models/account_invoice.py
@@ -38,7 +38,7 @@ class AccountMove(models.Model):
         price_unit_prec = self.env['decimal.precision'].precision_get('Product Price')
 
         for move in self:
-            if move.type not in ('in_invoice', 'in_refund', 'in_receipt') or not move.company_id.anglo_saxon_accounting:
+            if not move.is_purchase_document(include_receipts=True) or not move.company_id.anglo_saxon_accounting:
                 continue
 
             for line in move.invoice_line_ids:
